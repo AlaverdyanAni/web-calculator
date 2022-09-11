aa1a828563b4a36f1143d0314024b84fc4894482
Ani <ani-alaverdyan@mail.ru>
2022-09-11 19:56:59 +0300

    commit

--- src/main/java/pro/sky/java/course2/webcalculator/controller/CalculatorService.java
+++ src/main/java/pro/sky/java/course2/webcalculator/controller/CalculatorService.java
@@ -5,5 +5,5 @@ public interface CalculatorService {
    int  plus (Integer num1,Integer num2);
    int minus (Integer num1,Integer num2);
    int multiply (Integer num1,Integer num2);
-   double  (Integer num1,Integer num2);
+   double devide (Integer num1,Integer num2);
 }
