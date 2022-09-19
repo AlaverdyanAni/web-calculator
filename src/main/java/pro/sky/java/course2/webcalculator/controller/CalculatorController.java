3367feea94519ffc1839a152584616754365afdf
Ani <ani-alaverdyan@mail.ru>
2022-09-19 13:25:56 +0300

    commit1

--- src/main/java/pro/sky/java/course2/webcalculator/controller/CalculatorController.java
+++ src/main/java/pro/sky/java/course2/webcalculator/controller/CalculatorController.java
@@ -18,8 +18,8 @@ public class CalculatorController {
     }
 
     @GetMapping(path = "/calculator/plus")
-    public String plus(@RequestParam(name = "num1") Integer num1,
-                       @RequestParam(name = "num2") Integer num2) {
+    public String plus(@RequestParam(name = "num1", required = false) Integer num1,
+                       @RequestParam(name = "num2", required = false) Integer num2) {
         if (num1 == null || num2 == null)
             return "Введите цифру!";
         int plus = calculatorService.plus(num1, num2);
@@ -27,24 +27,24 @@ public class CalculatorController {
     }
 
     @GetMapping(path = "/calculator/minus")
-    public String minus(@RequestParam(name = "num1") Integer num1,
-                       @RequestParam(name = "num2") Integer num2) {
+    public String minus(@RequestParam(name = "num1", required=false) Integer num1,
+                       @RequestParam(name = "num2", required = false) Integer num2) {
         if (num1 == null || num2 == null)
             return "Введите цифру!";
         int minus = calculatorService.minus(num1, num2);
         return num1 + "-" + num2 + "=" + minus;
     }
     @GetMapping(path = "/calculator/multiply")
-    public String multiply(@RequestParam(name = "num1") Integer num1,
-                        @RequestParam(name = "num2") Integer num2) {
+    public String multiply(@RequestParam(name = "num1", required = false) Integer num1,
+                        @RequestParam(name = "num2", required = false) Integer num2) {
         if (num1 == null || num2 == null)
             return "Введите цифру!";
         int multiply = calculatorService.multiply(num1, num2);
         return num1 + "*" + num2 + "=" + multiply;
     }
     @GetMapping(path = "/calculator/divide")
-    public String divide(@RequestParam(name = "num1") Integer num1,
-                           @RequestParam(name = "num2") Integer num2) {
+    public String divide(@RequestParam(name = "num1", required = false) Integer num1,
+                           @RequestParam(name = "num2", required = false) Integer num2) {
         if (num1 == null || num2 == null)
             return "Введите цифру!";
         if (num2==0)
